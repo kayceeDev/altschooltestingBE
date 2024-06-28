c8e2777b157b37f13584f39e0575cc2611da63c9
kayceeDev <nwobodoe71@gmail.com>
2024-06-28 11:12:29 +0100

    Update rate limit

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ app.use(cors(corsOptions));
 
 // Rate limiting middleware
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  windowMs: 60 * 1000, // 15 minutes
+  max: 50, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   message: "Too many requests, please try again later.",
 });
 
